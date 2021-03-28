5582cc14136bc9684ebcb928cdaee8191c8957d1
Nasir <70495316+abdinaa17@users.noreply.github.com>
2021-03-27 23:53:29 -0500

    Update Header.js

--- Components/Header.js
+++ Components/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div className="Header-content">
             <h2 className="Header-title"  >doApp</h2>
             <small className="Header-small" >Simplify your day! </small>
-            <p className="Header-desc" >Increase your productivity by keeping your todos in one place!</p>
+            <p className="Header-desc" >Increase your productivity by keeping all of your todos in one place!</p>
         </div>
         
     </header> );
